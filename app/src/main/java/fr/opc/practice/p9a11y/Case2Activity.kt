d08d4ea65e575fc49ab241b8eff9e118e06906bd
Lucas Bigeard <luc.allau@gmail.com>
2024-11-25 12:48:28 +0100

    Case 2

--- app/src/main/java/fr/opc/practice/p9a11y/Case2Activity.kt
+++ app/src/main/java/fr/opc/practice/p9a11y/Case2Activity.kt
@@ -1,9 +1,11 @@
 package fr.opc.practice.p9a11y
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import fr.opc.practice.p9a11y.databinding.ActivityCase2Binding
+
 
 class Case2Activity : AppCompatActivity() {
     private lateinit var binding: ActivityCase2Binding
@@ -29,13 +31,28 @@ class Case2Activity : AppCompatActivity() {
         binding.recipeCard.setOnClickListener {
             // TODO navigate to recipe screen
         }
+
+        binding.favouriteButton.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_NO
+        binding.addRecipeToBasket.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_NO
+
+        binding.recipeCard.setOnClickListener {
+            binding.favouriteButton.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_YES
+            binding.addRecipeToBasket.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_YES
+            // TODO navigate to recipe screen
+        }
+
+
+
     }
 
     private fun setFavouriteButtonIcon(isFavourite: Boolean) {
         if (isFavourite) {
             binding.favouriteButton.setImageResource(R.drawable.ic_favourite_on)
+            binding.favouriteButton.contentDescription = getString(R.string.remove_from_favourites) // "Retirer des favoris"
         } else {
             binding.favouriteButton.setImageResource(R.drawable.ic_favourite_off)
+            binding.favouriteButton.contentDescription = getString(R.string.add_to_favourites) // "Ajouter aux favoris"
         }
     }
+
 }
