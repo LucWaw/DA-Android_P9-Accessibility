4500d4e180161fdbc191cc3cd0475936126a6d7a
Lucas Bigeard <luc.allau@gmail.com>
2024-11-26 12:56:14 +0100

    Case 2 : Accessibility Action

--- app/src/main/java/fr/opc/practice/p9a11y/Case2Activity.kt
+++ app/src/main/java/fr/opc/practice/p9a11y/Case2Activity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import fr.opc.practice.p9a11y.databinding.ActivityCase2Binding
+
 
 
 class Case2Activity : AppCompatActivity() {
@@ -21,37 +23,61 @@ class Case2Activity : AppCompatActivity() {
         binding.favouriteButton.setOnClickListener {
             isFavourite = !isFavourite
             setFavouriteButtonIcon(isFavourite)
+            updateAccessibilityAction(isFavourite) // Mettre à jour l'accessibilité
+
         }
 
         binding.addRecipeToBasket.setOnClickListener {
-            Toast.makeText(this, getString(R.string.recette_ajout_au_panier), Toast.LENGTH_SHORT)
-                .show()
+            addToBasket()
         }
 
-        binding.recipeCard.setOnClickListener {
-            // TODO navigate to recipe screen
-        }
+
 
         binding.favouriteButton.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_NO
         binding.addRecipeToBasket.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_NO
 
         binding.recipeCard.setOnClickListener {
-            binding.favouriteButton.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_YES
-            binding.addRecipeToBasket.importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_YES
             // TODO navigate to recipe screen
         }
 
+        ViewCompat.addAccessibilityAction(
+            binding.recipeCard,
+            getString(R.string.add_to_basket)
+        ) { _, _ ->
+            addToBasket()
+            true
+        }
 
+        updateAccessibilityAction(isFavourite)
+    }
 
+    private fun addToBasket() {
+        Toast.makeText(this, getString(R.string.recette_ajout_au_panier), Toast.LENGTH_SHORT)
+            .show()
     }
 
     private fun setFavouriteButtonIcon(isFavourite: Boolean) {
         if (isFavourite) {
             binding.favouriteButton.setImageResource(R.drawable.ic_favourite_on)
-            binding.favouriteButton.contentDescription = getString(R.string.remove_from_favourites) // "Retirer des favoris"
         } else {
             binding.favouriteButton.setImageResource(R.drawable.ic_favourite_off)
-            binding.favouriteButton.contentDescription = getString(R.string.add_to_favourites) // "Ajouter aux favoris"
+        }
+    }
+    private var accessibilityActionId: Int = View.NO_ID
+
+    fun updateAccessibilityAction(isFavourite: Boolean) {
+        // Supprime l'action précédente si elle existe
+        if (accessibilityActionId != View.NO_ID) {
+            ViewCompat.removeAccessibilityAction(binding.recipeCard, accessibilityActionId)
+        }
+
+        // Ajoute une nouvelle action avec un label adapté
+        accessibilityActionId = ViewCompat.addAccessibilityAction(
+            binding.recipeCard,
+            if (isFavourite) getString(R.string.remove_from_favourites) else getString(R.string.add_to_favourites)
+        ) { _, _ ->
+            binding.favouriteButton.performClick()
+            true
         }
     }
 
